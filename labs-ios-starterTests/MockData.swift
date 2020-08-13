b16efe4f994bc2a4389e5115ba6f3fab696b21c8
Hector <hectoremil21@hotmail.com>
2020-08-13 19:09:41 -0400

    Create MockData.swift. PropertyJSON mock data.

--- labs-ios-starterTests/MockData.swift
+++ labs-ios-starterTests/MockData.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+let propertyJSON: Data = """
+ {
+   "id": "11",
+   "name": "Trannsylvania",
+   "rooms": 12,
+   "phone": "123-456-7890",
+   "billingAddress": "420 Meta Dr.",
+   "shippingAddress": "420 Meta Dr.",
+   "coordinates": "100,100",
+   "shippingNote": "don't shake this bad boy",
+   "notes": "Very very fragile packages come from here.",
+   "users": [
+     {
+       "id": "11",
+       "firstName": "Test",
+       "lastName": "User"
+     }
+   ]
+ }
+""".data(using: .utf8)!
